3abfd6d74f1d04c08cbb53ef7b273a935bb0d65a
Kristen <kristenwoerdeman@gmail.com>
2025-03-27 18:02:29 -0700

    Update OpenSea link to collection URL

--- src/pages/api/mint.ts
+++ src/pages/api/mint.ts
@@ -60,7 +60,7 @@ export default async function handler(
           ` : `
           <meta property="fc:frame:button:1" content="View on OpenSea" />
           <meta property="fc:frame:button:1:action" content="link" />
-          <meta property="fc:frame:button:1:target" content="https://opensea.io/assets/ethereum/0x18f98DeeC72FA4EEa424a1E9F32dfFc83e4E0641/1" />
+          <meta property="fc:frame:button:1:target" content="https://opensea.io/collection/satoshe-slugger" />
           `}
         </head>
         <body>
